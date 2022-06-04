ec68d4efc08b962ff9f4ac6a39458da3af5838e8
My Le <hoangthimyleofficial1@gmail.com>
2022-06-04 23:06:46 +0700

    challenge1

---
+++ contract/target/wasm32-unknown-unknown/debug/greeter.d
@@ -0,0 +1,1 @@
+/home/kieuquan/Src/test/test-2/HelloWorldNear/contract/target/wasm32-unknown-unknown/debug/greeter.wasm: /home/kieuquan/Src/test/test-2/HelloWorldNear/contract/src/lib.rs
---
+++ src/App.js
@@ -0,0 +1,158 @@
+import 'regenerator-runtime/runtime'
+import React from 'react'
+import { login, logout } from './utils'
+import './global.css'
+
+import getConfig from './config'
+const { networkId } = getConfig(process.env.NODE_ENV || 'development')
+
+export default function App() {
+  // use React Hooks to store greeting in component state
+  const [greeting, set_greeting] = React.useState()
+
+  // when the user has not yet interacted with the form, disable the button
+  const [buttonDisabled, setButtonDisabled] = React.useState(true)
+
+  // after submitting the form, we want to show Notification
+  const [showNotification, setShowNotification] = React.useState(false)
+
+  // The useEffect hook can be used to fire side-effects during render
+  // Learn more: https://reactjs.org/docs/hooks-intro.html
+  React.useEffect(
+    () => {
+      // in this case, we only care to query the contract when signed in
+      if (window.walletConnection.isSignedIn()) {
+
+        // window.contract is set by initContract in index.js
+        window.contract.get_greeting({ account_id: window.accountId })
+          .then(greetingFromContract => {
+            set_greeting('')
+          })
+      }
+    },
+
+    // The second argument to useEffect tells React when to re-run the effect
+    // Use an empty array to specify "only run on first render"
+    // This works because signing into NEAR Wallet reloads the page
+    []
+  )
+
+  // if not signed in, return early with sign-in prompt
+  if (!window.walletConnection.isSignedIn()) {
+    return (
+      <div style={{ width: '100%', height: 'calc(100vh - 45px)', display: 'flex', flexDirection: 'column', justifyContent: 'center',alignItems: 'center'}}>
+        <img style={{borderRadius:'8px 8px 0 0'}} src="https://scontent.fhan4-3.fna.fbcdn.net/v/t1.15752-9/279164185_1198347830992390_5079704922099714452_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=ae9488&_nc_ohc=JRhbNXyodvcAX-mjO0T&_nc_ht=scontent.fhan4-3.fna&oh=03_AVI6Xxw1GUzMEQrnEltEMl_TPGiT-Dk_NasWkt_vXZlEUg&oe=62B9A415" />
+        <button style={{ width:'700px', borderRadius: '0 0 8px 8px' }} onClick={login}>Sign in</button>
+      </div>
+    )
+  }
+
+  return (
+    // use React Fragment, <>, to avoid wrapping elements in unnecessary divs
+    <>
+      <button className="link" style={{ float: 'right', display: 'flex', alignItems: 'center'}} onClick={logout}>
+        <img style={{ width:'20px', marginLeft: '10px'}}src="https://iconarchive.com/download/i91934/icons8/windows-8/User-Interface-Logout.ico"/>
+      </button>
+      <main>
+        <h1 style={{ marginBottom:'0'}}>
+          Welcome {window.accountId}!
+        </h1>
+        <form onSubmit={async event => {
+          event.preventDefault()
+
+          // get elements from the form using their id attribute
+          const { fieldset, greeting } = event.target.elements
+
+          // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
+          const newGreeting = greeting.value
+
+          // disable the form while the value gets updated on-chain
+          fieldset.disabled = true
+
+          try {
+            // make an update call to the smart contract
+            await window.contract.set_greeting({
+              // pass the value that the user entered in the greeting field
+              message: newGreeting
+            })
+          } catch (e) {
+            alert(
+              'Something went wrong! ' +
+              'Maybe you need to sign out and back in? ' +
+              'Check your browser console for more info.'
+            )
+            throw e
+          } finally {
+            // re-enable the form, whether the call succeeded or failed
+            fieldset.disabled = false
+          }
+
+          // update local `greeting` variable to match persisted value
+          set_greeting(newGreeting)
+
+          // show Notification
+          setShowNotification(true)
+
+          // remove Notification again after css animation completes
+          // this allows it to be shown again next time the form is submitted
+          setTimeout(() => {
+            setShowNotification(false)
+          }, 11000)
+        }}>
+          <fieldset id="fieldset">
+            <label
+              htmlFor="greeting"
+              style={{
+                display: 'block',
+                color: 'var(--gray)',
+                marginBottom: '0.5em'
+              }}
+            >
+              What is your name?
+            </label>
+            <div style={{ display: 'flex' }}>
+              <input
+                autoComplete="off"
+                defaultValue={greeting}
+                id="greeting"
+                placeholder='...'
+                onChange={e => setButtonDisabled(e.target.value === greeting)}
+                style={{ flex: 1 }}
+              />
+              <button
+                disabled={buttonDisabled}
+                style={{ borderRadius: '0 5px 5px 0' }}
+              >
+                Send
+              </button>
+            </div>
+          </fieldset>
+        </form>
+        <h3>Greeting: { greeting ? `"Hello ${greeting}! Have a nice day."` : '' }</h3>
+      </main>
+      {/* {showNotification && <Notification />} */}
+    </>
+  )
+}
+
+// this component gets rendered by App after the form is submitted
+function Notification() {
+  const urlPrefix = `https://explorer.${networkId}.near.org/accounts`
+  return (
+    <aside>
+      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.accountId}`}>
+        {window.accountId}
+      </a>
+      {' '/* React trims whitespace around tags; insert literal space character when needed */}
+      called method: 'set_greeting' in contract:
+      {' '}
+      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.contract.contractId}`}>
+        {window.contract.contractId}
+      </a>
+      <footer>
+        <div>✔ Succeeded</div>
+        <div>Just now</div>
+      </footer>
+    </aside>
+  )
+}
